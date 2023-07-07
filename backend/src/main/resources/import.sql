72934b5a3b42cd46fc014eb96622ed97aeb8a3da
Lázaro <lazaronobrega3@gmail.com>
2023-07-07 16:27:57 -0300

    Povoando o banco para testes

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_contato(nome,endereco,data_nascimento,telefone) VALUES ('Barry Allen','Rua A','2022-06-16', '8398363-3637');
+INSERT INTO tb_contato(nome,endereco,data_nascimento,telefone) VALUES ('Boby Brayan','Rua B','2022-12-05', '8398363-9643');
+INSERT INTO tb_contato(nome,endereco,data_nascimento,telefone) VALUES ('Lázaro Fonseca','Rua C','2022-05-23', '8398363-0831');
+INSERT INTO tb_contato(nome,endereco,data_nascimento,telefone) VALUES ('Gesica Souza','Rua D','2000-11-18', '8398363-9067');
+INSERT INTO tb_contato(nome,endereco,data_nascimento,telefone) VALUES ('Maria José','Rua E','1990-10-12', '8398363-5634');
+INSERT INTO tb_contato(nome,endereco,data_nascimento,telefone) VALUES ('Sebastião da Silva','Rua F','1949-08-25', '8396783-9830');
